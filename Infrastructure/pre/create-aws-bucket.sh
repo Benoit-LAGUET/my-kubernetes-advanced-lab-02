014660a1ee04f447b338fd7919238e40e793a04b
Benoit Laguet <laguet.benoit@gmail.com>
2021-07-22 17:44:45 +0200

    add test

--- Infrastructure/pre/create-aws-bucket.sh
+++ Infrastructure/pre/create-aws-bucket.sh
@@ -1,15 +1,25 @@
 #/bin/bash
-BucketName=lab-terraformstate-bucket-01
+BucketName=lab-terraformstate-bucket-02
 Region=eu-central-1
 
-aws s3 mb s3://$BucketName \
---region $Region
+AwsAccountId=`aws sts get-caller-identity --query 'Account' --output text`
+aws s3api head-bucket --bucket $BucketName  --expected-bucket-owner $AwsAccountId >/dev/null
+BucketExist=$?
 
-aws s3api put-bucket-versioning \
-    --bucket $BucketName \
-    --versioning-configuration Status=Enabled 
+if [ $BucketExist -ne 0 ]
+then
+    aws s3 mb s3://$BucketName \
     --region $Region
 
-aws s3api put-public-access-block \
-    --bucket lab-terraformstate-bucket-01 \
+    aws s3api put-bucket-versioning \
+    --bucket $BucketName \
+    --versioning-configuration Status=Enabled \
+    --region $Region
+
+    aws s3api put-public-access-block \
+    --bucket $BucketName \
     --public-access-block-configuration '{"BlockPublicAcls": true, "IgnorePublicAcls": true, "BlockPublicPolicy": true, "RestrictPublicBuckets": true}'
+else
+    echo "Bucket $BucketName already exist"
+fi
+
