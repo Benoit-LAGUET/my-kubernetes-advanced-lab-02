3b752ccf949ef3468da78b94cfab2227af3a3b00
Benoit Laguet <laguet.benoit@gmail.com>
2021-07-28 09:09:25 +0200

    fix path

--- install.sh
+++ install.sh
@@ -1,21 +1,19 @@
 #/bin/bash
 
-scriptPath=`pwd`
 dirName=`dirname $0`
 echo $dirName
 
-echo "Move to the script directory $scriptPath"
+echo "Move to the script directory $dirName"
 cd $dirName
 
 echo "Create a AWS S3 bucket to store the terraform state"
-$scriptPath/Infrastructure/pre/create-aws-bucket.sh
+$dirName/Infrastructure/pre/create-aws-bucket.sh
 
 echo "Deploy EKS cluster using terraform"
-cd $dirName/Infrastructure/terraform
-terraform apply
+terraform -chdir=$dirName/Infrastructure/terraform apply
 
 echo "Update Kubconfig"
-$dirName/Infrastructure/post/setContext.sh
+$dirName/Infrastructure/post/setContext.sh $dirName/Infrastructure/terraform/
 
 echo "Deploy k8s-spot-termination-handler"
 $dirName/Apps/k8s-spot-termination-handler/install-k8s-spot-termination-handler.sh
